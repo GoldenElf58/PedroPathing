2140cbc67cbe238485adbbb8fa3a180c48ebc7c5
GoldenElf58 <91762091+GoldenElf58@users.noreply.github.com>
2025-10-16 20:10:11 -0400

    Fix totalHeading bug in OTOSLocalizer
    Each update loop, totalHeading would always increase because it was incremented by getSmallestAngleDifference, which is always positive. This multiplies that by the turn direction, so that totalHeading can decrease and work as intended (that being a heading number that wraps past +/-180°)

--- ftc/src/main/java/com/pedropathing/ftc/localization/localizers/OTOSLocalizer.java
+++ ftc/src/main/java/com/pedropathing/ftc/localization/localizers/OTOSLocalizer.java
@@ -134,7 +134,7 @@ public class OTOSLocalizer implements Localizer {
     @Override
     public void update() {
         otos.getPosVelAcc(otosPose,otosVel,otosAcc);
-        totalHeading += MathFunctions.getSmallestAngleDifference(otosPose.h, previousHeading);
+        totalHeading += MathFunctions.getSmallestAngleDifference(otosPose.h, previousHeading) * MathFunctions.getTurnDirection(previousHeading, otosPose.h);
         previousHeading = otosPose.h;
     }
 
